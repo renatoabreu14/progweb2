cae6f6700f0fe86f3cf1935de2c1753be6dbdbb1
renatoabreu14 <renato.abreu.info@gmail.com>
2019-07-02 22:07:37 -0300

    Aula do dia 02/07/2019 Script do BD

--- pw2_2019.sql
+++ pw2_2019.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 28, 2019 at 10:01 PM
--- Server version: 10.1.38-MariaDB-0ubuntu0.18.04.1
--- PHP Version: 7.2.17-0ubuntu0.18.04.1
+-- Generation Time: Jul 02, 2019 at 10:06 PM
+-- Server version: 10.1.40-MariaDB-0ubuntu0.18.04.1
+-- PHP Version: 7.2.19-0ubuntu0.18.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -86,6 +86,31 @@ INSERT INTO `genero` (`id`, `nome`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `itemvenda`
+--
+
+CREATE TABLE `itemvenda` (
+  `id` int(11) NOT NULL,
+  `fk_idvenda` int(11) NOT NULL,
+  `fk_idlivro` int(11) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `valorlivro` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `itemvenda`
+--
+
+INSERT INTO `itemvenda` (`id`, `fk_idvenda`, `fk_idlivro`, `quantidade`, `valorlivro`) VALUES
+(1, 10, 1, 2, 100),
+(2, 11, 1, 2, 100),
+(3, 12, 1, 2, 100),
+(4, 13, 1, 2, 100),
+(5, 14, 1, 2, 100);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `livro`
 --
 
@@ -108,6 +133,48 @@ CREATE TABLE `livro` (
 INSERT INTO `livro` (`id`, `titulo`, `descricao`, `autor`, `idgenero`, `ideditora`, `valor`, `ano`, `capa`) VALUES
 (1, 'Senhor dos Anéis', 'sdfadgdfagdafgfa', 'Fulano', 2, 1, 100, 1987, '07ff3c9d607cd5060badc70c938e9dd4.jpg'),
 (17, 'Game of Thrones', 'sfsdfsdfs', 'George R. R. Martin', 3, 2, 100, 2001, 'c339e0fe27fe7a68d739bd5dcc22d7f1.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `situacao`
+--
+
+CREATE TABLE `situacao` (
+  `id` int(11) NOT NULL,
+  `descricao` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `situacao`
+--
+
+INSERT INTO `situacao` (`id`, `descricao`) VALUES
+(1, 'Aguardando confirmação de pagamento');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `venda`
+--
+
+CREATE TABLE `venda` (
+  `id` int(11) NOT NULL,
+  `fk_idcliente` int(11) NOT NULL,
+  `datavenda` date NOT NULL,
+  `fk_idsituacao` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `venda`
+--
+
+INSERT INTO `venda` (`id`, `fk_idcliente`, `datavenda`, `fk_idsituacao`) VALUES
+(10, 1, '2019-07-02', 1),
+(11, 1, '2019-07-02', 1),
+(12, 1, '2019-07-02', 1),
+(13, 1, '2019-07-02', 1),
+(14, 1, '2019-07-02', 1);
 
 --
 -- Indexes for dumped tables
@@ -132,12 +199,34 @@ ALTER TABLE `genero`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `itemvenda`
+--
+ALTER TABLE `itemvenda`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_idvenda` (`fk_idvenda`),
+  ADD KEY `fk_idproduto` (`fk_idlivro`);
+
+--
 -- Indexes for table `livro`
 --
 ALTER TABLE `livro`
   ADD PRIMARY KEY (`id`),
   ADD KEY `idgenero` (`idgenero`),
   ADD KEY `ideditora` (`ideditora`);
+
+--
+-- Indexes for table `situacao`
+--
+ALTER TABLE `situacao`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `venda`
+--
+ALTER TABLE `venda`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_idcliente` (`fk_idcliente`),
+  ADD KEY `fk_idsituacao` (`fk_idsituacao`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -159,13 +248,35 @@ ALTER TABLE `editora`
 ALTER TABLE `genero`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
+-- AUTO_INCREMENT for table `itemvenda`
+--
+ALTER TABLE `itemvenda`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
 -- AUTO_INCREMENT for table `livro`
 --
 ALTER TABLE `livro`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 --
+-- AUTO_INCREMENT for table `situacao`
+--
+ALTER TABLE `situacao`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `venda`
+--
+ALTER TABLE `venda`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+--
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `itemvenda`
+--
+ALTER TABLE `itemvenda`
+  ADD CONSTRAINT `fk_itemvenda_livro` FOREIGN KEY (`fk_idlivro`) REFERENCES `livro` (`id`),
+  ADD CONSTRAINT `fk_itemvenda_venda` FOREIGN KEY (`fk_idvenda`) REFERENCES `venda` (`id`);
 
 --
 -- Constraints for table `livro`
@@ -173,6 +284,13 @@ ALTER TABLE `livro`
 ALTER TABLE `livro`
   ADD CONSTRAINT `fk_livro_editora` FOREIGN KEY (`ideditora`) REFERENCES `editora` (`id`),
   ADD CONSTRAINT `fk_livro_genero` FOREIGN KEY (`idgenero`) REFERENCES `genero` (`id`);
+
+--
+-- Constraints for table `venda`
+--
+ALTER TABLE `venda`
+  ADD CONSTRAINT `fk_venda_cliente` FOREIGN KEY (`fk_idcliente`) REFERENCES `cliente` (`id`),
+  ADD CONSTRAINT `fk_venda_situacao` FOREIGN KEY (`fk_idsituacao`) REFERENCES `situacao` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
